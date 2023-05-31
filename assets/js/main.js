048ff7bbd87ac34acd2623af90d8709d303b7c79
IlmanNashran <ilmannashran@gmail.com>
2023-05-31 15:49:39 +0800

    buat slider swiper

--- assets/js/main.js
+++ assets/js/main.js
@@ -97,7 +97,19 @@ const modelViews = document.querySelectorAll('.services_model'),
        })
       })
  /*==================== PORTFOLIO SWIPER  ====================*/
+ let swiper = new Swiper('.portfolio__container', {
+    cssMode: true,
+    loop:true,
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+    pagination: {
+      el: '.swiper-pagination',
+      clickable:true,
+    },
 
+  });
 
 /*==================== TESTIMONIAL ====================*/
 
